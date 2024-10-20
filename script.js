e90836bd27f7a3cb9ff5c41f8e4a7e2fefbac324
Iryna Glytsko <82415832+IrynaGlytsko@users.noreply.github.com>
2024-10-20 18:27:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+$(function() {
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+
+    checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+        checkScroll(scrollOffset);
+});
+
+function checkScroll(scrollOffset) {
+    if(scrollOffset >= introH) {
+        header.addClass("fixed");
+    } else {
+        header.removeClass("fixed");
+    }
+}
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+            $("#nav a").removeClass("active");
+            $this.addClass("active");
+
+            $("html, body").animate({
+                scrollTop: blockOffset
+            }, 500);
+    });
+
+    $("#nav_toggle").on("click", function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+
+    $("[data-slider]").slick({
+        infinity: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
